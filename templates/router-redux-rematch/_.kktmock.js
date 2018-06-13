00b1f1f8e3cd1367292e487900a782ec62ad631e
jaywcjlove <398188662@qq.com>
2018-06-14 01:28:52 +0800

    Update templates mock data.

--- templates/router-redux-rematch/_.kktmock.js
+++ templates/router-redux-rematch/_.kktmock.js
@@ -1,36 +1,21 @@
 const proxy = {
-  'GET /api/userinfo/:id': (req, res) => {
-    // console.log('---->', req.params);
-    return res.json({
-      id: 1,
-      username: 'kenny',
-      sex: 6,
-    });
-  },
-  'POST /api/login/account': (req, res) => {
+  'GET /api/user': { id: 1, username: 'kenny', sex: 6 },
+  'POST /api/login': (req, res) => {
     const { password, username } = req.body;
-    if (password === '888888' && username === 'admin') {
+    if (password === 'KKT' && username === 'KKT') {
       return res.json({
-        status: 'ok',
-        code: 0,
-        token: 'sdfsdfsdfdsf',
-        data: {
-          id: 1,
-          username: 'kenny',
-          sex: 6,
-        },
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+        username: 'KKT',
       });
     }
-    return res.json({
-      status: 'error',
-      code: 403,
+    return res.status(401).json({
+      error: 'bad username/password, access denied',
     });
   },
-  'DELETE /api/user/:id': (req, res) => {
-    // console.log('---->', req.body);
-    // console.log('---->', req.params.id);
-    res.send({ status: 'ok', message: '删除成功！' });
-  },
+  'GET /api/user/list': [
+    { id: 1, username: 'kenny', sex: 6 },
+    { id: 2, username: 'kenny', sex: 6 },
+  ],
 };
 
 module.exports = proxy;
