5aa9884cd667e1e95663609b45e31b7171c1289b
jaywcjlove <398188662@qq.com>
2018-06-23 16:26:18 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,19 +30,6 @@ const eslintrc = {
     ],
     "function-paren-newline": 0,
     "class-methods-use-this": 0,
-    "react/sort-comp": 0,
-    "react/prop-types": 0,
-    "react/jsx-first-prop-new-line": 0,
-    "react/require-extension": 0,
-    "react/jsx-filename-extension": [
-      1,
-      {
-        "extensions": [
-          ".js",
-          ".jsx"
-        ]
-      }
-    ],
     "import/extensions": 0,
     "import/no-unresolved": 0,
     "import/no-extraneous-dependencies": 0,
@@ -54,6 +41,14 @@ const eslintrc = {
     "jsx-a11y/label-has-for": 0,
     "jsx-a11y/no-noninteractive-element-interactions": 0,
     "jsx-a11y/mouse-events-have-key-events": 0,
+    "react/sort-comp": 0,
+    "react/prop-types": 0,
+    "react/jsx-first-prop-new-line": 0,
+    "react/require-extension": 0,
+    "react/jsx-filename-extension": [ 1,
+      { "extensions": [ ".js", ".jsx" ] }
+    ],
+    "react/no-unescaped-entities": 0,
     "react/no-danger": 0,
     "react/jsx-no-bind": 0,
     "react/forbid-prop-types": 0,
