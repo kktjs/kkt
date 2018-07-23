d624df017d873f5c4bbe4d1a83f88375961cbb19
jaywcjlove <398188662@qq.com>
2018-07-23 20:52:15 +0800

    Modify webpack config.

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -48,7 +48,35 @@ module.exports = {
             ],
           },
           {
-            test: /\.(css|less)$/,
+            test: /\.(css)$/,
+            use: [
+              require.resolve('style-loader'),
+              require.resolve('css-loader'),
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'), // eslint-disable-line
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              },
+              require.resolve('less-loader'),
+            ],
+          },
+          {
+            test: /\.(less)$/,
             use: [
               require.resolve('style-loader'),
               {
