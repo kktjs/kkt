dfaa68e587802d96b5cee7898e8ba56ba9ce812f
jaywcjlove <398188662@qq.com>
2018-08-17 19:31:05 +0800

    Update eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,6 +43,7 @@ const eslintrc = {
     "jsx-a11y/click-events-have-key-events": 0,
     "jsx-a11y/anchor-is-valid": 0,
     "jsx-a11y/label-has-for": 0,
+    "jsx-a11y/label-has-associated-control": 0,
     "jsx-a11y/no-noninteractive-element-interactions": 0,
     "jsx-a11y/mouse-events-have-key-events": 0,
     "react/sort-comp": 0,
