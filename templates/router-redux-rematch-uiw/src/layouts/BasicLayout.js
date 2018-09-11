97e16c78432265c4a1518ff1be4b9be8260d12c3
jaywcjlove <398188662@qq.com>
2018-09-12 02:04:27 +0800

    Fix errors caused by copy tamplates.

--- templates/router-redux-rematch-uiw/src/layouts/BasicLayout.js
+++ templates/router-redux-rematch-uiw/src/layouts/BasicLayout.js
@@ -35,19 +35,15 @@ class BasicLayout extends PureComponent {
       if (path === '/') {
         RouteComponents.push(<Route exact key={idx + 1} path="/" render={() => <Redirect to="/home" />} />);
       } else {
+        const ChildComponent = (props) => {
+          const ChildComp = routerData[path].component;
+          // 可以给子组件传一些参数如： isNavShow=true
+          return (
+            <ChildComp {...props} isNavShow />
+          );
+        };
         RouteComponents.push(
-          <Route
-            exact
-            key={idx + 1}
-            path={path}
-            render={(props) => {
-              const ChildComp = routerData[path].component;
-              // 可以给子组件传一些参数如： isNavShow=true
-              return (
-                <ChildComp {...props} isNavShow />
-              );
-            }}
-          />
+          <Route exact key={idx + 1} path={path} render={ChildComponent} />
         );
       }
     });
