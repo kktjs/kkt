30a4902ff8f49c17b64c6ad2124fb914fb4e8127
jaywcjlove <398188662@qq.com>
2019-01-06 02:43:08 +0800

    Update default example.

--- example/default/src/App.js
+++ example/default/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-// import styles from './App.module.css';
-import styles from './index.module.css';
-import './index.css';
+import logo from './logo.svg';
+import styles from './App.module.css';
+import './App.css';
 
 const App = () => (
-  <div className={styles.warpper}>
-    Welcome to KKT.
-    <br />
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p className={styles.warpper}>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
   </div>
 );
 
