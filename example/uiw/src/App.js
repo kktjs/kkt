fb0d47e550ed308ad107fc1423f68b04870ea0f6
jaywcjlove <398188662@qq.com>
2019-09-08 18:30:21 +0800

    Add uiw example.

---
+++ example/uiw/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Button } from 'uiw';
+import logo from './logo.svg';
+import styles from './App.module.css';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p className={styles.warpper}>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <div className={styles.btns}>
+        <Button type="primary">主要按钮</Button>
+        <Button type="success">成功按钮</Button>
+        <Button type="warning">警告按钮</Button>
+        <Button type="danger">错误按钮</Button>
+        <Button type="light">亮按钮</Button>
+        <Button type="dark">暗按钮</Button>
+      </div>
+    </header>
+  </div>
+);
+
+export default App;
