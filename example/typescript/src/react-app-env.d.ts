5536f01944b10721d9613e3c183d46cb3059b7e4
nihao <nihao@nihaodeMacBook-Air.local>
2020-12-21 19:30:57 +0800

    type: Fix type erros.

--- example/typescript/src/react-app-env.d.ts
+++ example/typescript/src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module '*.module.less' {
+    const classes: { readonly [key: string]: string };
+    export default classes;
+}
+
+declare module '*.less' {
+    const classes: { readonly [key: string]: string };
+    export default classes;
+}
