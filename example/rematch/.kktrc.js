37a76a309b9c30921430b3f8770c906a5dff5d20
jaywcjlove <398188662@qq.com>
2020-12-22 00:06:27 +0800

    chore(rematch): update kktrc config.

--- example/rematch/.kktrc.js
+++ example/rematch/.kktrc.js
@@ -17,7 +17,11 @@ export const devServer = (configFunction) => (proxy, allowedHost) => {
   // Create the default config by calling configFunction with the proxy/allowedHost parameters
   let config = configFunction(proxy, allowedHost);
 
-  config = apiMocker(config, path.resolve('./mocker/index.js'));
-  // Return your customised Webpack Development Server config.
+  config = apiMocker(config, path.resolve('./mocker/index.js'), {
+    proxy: {
+      '/repos/(.*)': 'https://api.github.com/',
+    },
+    changeHost: true,
+  });
   return config;
 }
