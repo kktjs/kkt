7e4d106d48702fba45471f0d3c7f057eeb2af569
jaywcjlove <398188662@qq.com>
2020-12-26 00:45:31 +0800

    fix(deps): update dependency react-dynamic-loadable to v2.0.5 #128

--- example/rematch-tsx/src/react-app-env.d.ts
+++ example/rematch-tsx/src/react-app-env.d.ts
@@ -4,13 +4,3 @@ declare module '*.module.less' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
-
-declare module 'react-dynamic-loadable' {
-  interface Options {
-    models: () => Promise<any>[],
-    component: () => Promise<any>,
-    LoadingComponent: () => JSX.Element,
-  }
-  function _default(options: Options): void;
-  export default _default;
-}
