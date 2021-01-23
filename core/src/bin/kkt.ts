6d7cf79f5733d48a7c4b5ba965f3e4b608b9e266
jaywcjlove <398188662@qq.com>
2021-01-23 23:39:21 +0800

    fix: The kkt command add .catch handle.

--- core/src/bin/kkt.ts
+++ core/src/bin/kkt.ts
@@ -6,8 +6,8 @@ function help() {
   console.log('\n  Usage: kkt [start|build|test] [--help|h]');
   console.log('\n  Displays help information.');
   console.log('\n  Options:\n');
-  console.log('    --version, -v', 'Show version number');
-  console.log('    --help, -h', 'Displays help information.');
+  console.log('    --version, -v,', 'Show version number');
+  console.log('    --help, -h,', 'Displays help information.');
   console.log('    --app-src,', 'Specify the entry directory.');
   console.log('    --no-open-browser,', 'Do not open in browser.');
   console.log('    --no-clear-console,', 'Do not clear the command line information.');
@@ -22,30 +22,34 @@ function help() {
   console.log('  $ \x1b[35mkkt\x1b[0m test');
 }
 
-(async () => {
-  const args = process.argv.slice(2);
-  const argvs = minimist(args);
-  if (argvs.h || argvs.help) {
-    return help();
-  }
-  const { version } = require('../../package.json');
-  if (argvs.v || argvs.version) {
-    console.log(`\n create-kkt v${version}\n`);
-    return;
-  }
-  const scriptName = argvs._[0];
-  if (scriptName && /(^build|start|test)$/.test(scriptName)) {
-    if (scriptName === 'test') {
-      await require('../scripts/testk')(argvs);
-    } else {
-      await require(`../scripts/${scriptName}`)(argvs);
+;(async () => {
+  try {
+    const args = process.argv.slice(2);
+    const argvs = minimist(args);
+    if (argvs.h || argvs.help) {
+      return help();
     }
-  } else {
-    console.log(`Unknown script "${scriptName}".`);
-    console.log('Perhaps you need to update react-scripts?');
-    console.log(
-      'See: https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#updating-to-new-releases',
-    );
-    help();
+    if (argvs.v || argvs.version) {
+      const { version } = require('../../package.json');
+      console.log(`\n create-kkt v${version || ''}\n`);
+      return;
+    }
+    const scriptName = argvs._[0];
+    if (scriptName && /(^build|start|test)$/.test(scriptName)) {
+      if (scriptName === 'test') {
+        await require('../scripts/testk')(argvs);
+      } else {
+        await require(`../scripts/${scriptName}`)(argvs);
+      }
+    } else {
+      console.log(`Unknown script "\x1b[1;37m${scriptName}\x1b[0m".`);
+      console.log('Perhaps you need to update react-scripts?');
+      console.log(
+        'See: https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#updating-to-new-releases',
+      );
+      help();
+    }
+  } catch (error) {
+    console.log('\x1b[31m KKT:ERROR:\x1b[0m', error);
   }
 })();
