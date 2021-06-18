d16df8d0bbeb1f0eecc6a7ceb5eb85bbfb6b724e
jaywcjlove <398188662@qq.com>
2021-06-19 00:16:07 +0800

    fix(kkt): Fix test app-src errors.

--- core/src/scripts/testk.ts
+++ core/src/scripts/testk.ts
@@ -1,10 +1,17 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'test';
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'test';
+process.env.NODE_ENV = 'test';
+process.env.PUBLIC_URL = '';
 
 import { reactScripts } from '../utils/path';
+import { overridePaths } from '../overrides/paths';
 import { TestArgs } from '..';
 
 export default async function test(argvs: TestArgs) {
   try {
+    // Ensure environment variables are read.
+    require(`${reactScripts}/config/env`);
+    await overridePaths(argvs);
     // run original script
     require(`${reactScripts}/scripts/test`);
   } catch (error) {
