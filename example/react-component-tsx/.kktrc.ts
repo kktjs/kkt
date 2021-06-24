ecba4ea6735d9c0f8bbb850d922dc97f4cad478c
小弟调调™ <kennyiseeyou@gmail.com>
2021-06-24 13:20:53 +0800

    chore: Update .kktrc.ts

--- example/react-component-tsx/.kktrc.ts
+++ example/react-component-tsx/.kktrc.ts
@@ -20,7 +20,9 @@ export default (conf: Configuration, env: 'development' | 'production', options:
       VERSION: JSON.stringify(pkg.version),
     }),
   );
-  conf.output = { ...conf.output, publicPath: './' };
+  if (env === 'production') {
+    conf.output = { ...conf.output, publicPath: './' };
+  }
   return conf;
 };
 
