ed69f77d01998585fe23cf9f16531630980f64e2
jaywcjlove <398188662@qq.com>
2021-12-30 15:59:08 +0800

    chore: Delete the rematch-uiw example.

--- example/rematch-uiw/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { init } from '@rematch/core';
-import loadingPlugin from '@rematch/loading';
-import * as models from '../models/global';
-
-export const store = init({
-  models,
-  plugins: [loadingPlugin()],
-});
