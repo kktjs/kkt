df76ab3c026e3b6ef47416dac0df0ef264b1eddb
jaywcjlove <398188662@qq.com>
2021-12-30 10:20:38 +0800

    chore(react-router): Update react-router example.

--- example/react-router/src/routes/help/dashboard/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './index.module.less';
-
-export default function Dashboard() {
-  return <div className={styles.warpper}>帮助页面首页</div>;
-}
---
+++ example/react-router/src/routes/help/dashboard/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HelpDashboard() {
+  return <div>Help Page.</div>;
+}
