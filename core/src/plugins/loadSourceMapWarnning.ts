bc9b39be57f650fd8f45218f5f070c7d65674010
jaywcjlove <398188662@qq.com>
2022-01-01 22:52:27 +0800

    fix: Fix filter sourceMapping Url errors. #325

--- core/src/plugins/loadSourceMapWarnning.ts
+++ core/src/plugins/loadSourceMapWarnning.ts
@@ -2,6 +2,10 @@ import FS from 'fs-extra';
 import { Configuration } from 'webpack';
 import { LoaderConfOptions } from '../utils/loaderConf';
 
+/**
+ * sourceMap source file does not exist #325
+ * https://github.com/kktjs/kkt/issues/325
+ */
 export function loadSourceMapWarnning(
   conf: Configuration,
   env: 'development' | 'production',
@@ -13,7 +17,7 @@ export function loadSourceMapWarnning(
       // ;(conf.module.rules[0] as any).exclude = /((@babel(?:\/|\\{1,2})runtime)|codesandbox-import-utils)/;
       (conf.module.rules[0] as any).options = {
         filterSourceMappingUrl: (url: string, resourcePath: string) => {
-          if (/\.(js|jsx|ts|tsx)$/.test(resourcePath) && !FS.existsSync(resourcePath)) {
+          if (/\.(js|mjs|jsx|ts|tsx|css)$/.test(resourcePath) && !FS.existsSync(resourcePath)) {
             return 'skip';
           }
           return true;
