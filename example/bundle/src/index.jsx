38224d394d631eb1b1bb47136031b8c04f1c3b48
jaywcjlove <398188662@qq.com>
2022-03-07 18:28:19 +0800

    clean(ncc): move packages/ncc -> kktjs/ncc

--- example/bundle/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './app/App';
-import './index.css';
-
-ReactDOM.render(<div>test app</div>, document.getElementById('root'));
-// ReactDOM.render(<App />, document.getElementById('root'));
