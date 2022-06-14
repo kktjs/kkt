44aab666e099a0a7d2fd421b17b0775980923ec2
jaywcjlove <398188662@qq.com>
2022-06-15 01:48:51 +0800

    fix(rematch-tsx): fix example error.

--- example/rematch-tsx/src/index.tsx
+++ example/rematch-tsx/src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter, useRoutes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './models';
