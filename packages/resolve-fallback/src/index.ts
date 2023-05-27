36f644dac16dbb47d50822c532efd914825313fb
jaywcjlove <398188662@qq.com>
2023-05-28 01:59:00 +0800

    fix: fix can't resolve 'process/browser' issue. #412

--- packages/resolve-fallback/src/index.ts
+++ packages/resolve-fallback/src/index.ts
@@ -8,13 +8,19 @@ export default function resolveFallback(conf: Configuration): Configuration {
     assert: require.resolve('assert'),
     buffer: require.resolve('buffer'),
     fs: false,
-    path: false,
+    path: require.resolve('path-browserify'),
     crypto: require.resolve('crypto-browserify'),
     http: require.resolve('stream-http'),
     https: require.resolve('https-browserify'),
     os: require.resolve('os-browserify/browser'),
     stream: require.resolve('stream-browserify'),
     url: require.resolve('url'),
+  });
+  conf.module.rules.unshift({
+    test: /\.m?js$/,
+    resolve: {
+      fullySpecified: false, // disable the behavior
+    },
   });
   conf.plugins = (conf.plugins || []).concat([
     new webpack.ProvidePlugin({
